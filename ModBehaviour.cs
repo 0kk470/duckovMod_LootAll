f5d01f2f994e65caca718888a6d96140e55bc768
0kk470 <2549366978@qq.com>
2025-10-18 19:02:20 +0800

    first commit

---
+++ ModBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using Duckov.UI;
+using UnityEngine.UI;
+
+namespace LootAll
+{
+
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        private Button pickAllButton;
+
+        void Awake()
+        {
+            LootView.onOpen += OnLootViewOpened;
+        }
+        void OnDestroy()
+        {
+            LootView.onOpen -= OnLootViewOpened;
+        }
+
+        void OnLootViewOpened(ManagedUIElement lootView)
+        {
+            FindPickAllButton();
+            Invoke("RefreshButton", 0.05f);
+        }
+
+        private void FindPickAllButton()
+        {
+            if (pickAllButton != null)
+                return;
+            var fieldInfo = typeof(LootView).GetField("pickAllButton",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            pickAllButton = fieldInfo.GetValue(LootView.Instance) as Button;
+        }
+
+        private void RefreshButton()
+        {
+            if (pickAllButton != null)
+            {
+                var inventory = LootView.Instance.TargetInventory;
+                pickAllButton.gameObject.SetActive(inventory != null);
+                bool interactable = inventory?.GetItemCount() > 0;
+                pickAllButton.interactable = interactable;
+            }
+        }
+    }
+}
